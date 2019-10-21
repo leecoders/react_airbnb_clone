88072842c1b038bd02f3970629169e2e07b1cbdd
leecoders <als-1994@hanmail.net>
2019-10-21 11:08:49 +0900

    test: Nav 컴포넌트 -> 버튼 이동 마우스 이벤트 -> 드래그 이벤트로 변경 시도

---
+++ docs/week8_feedback.md
@@ -0,0 +1,15 @@
+## 알게된 내용
+
+### input 태그의 type을 `number`로 하면 숫자 또는 `+`, `-`, `.`만을 입력 받을 수 있다.
+- 하지만 `+`, `-`, `.` 또한 쓸모 없는데.. 해결 방법을 찾아봐야 함
+- (추가) `e`도 입력된다..
+
+### input 태그의 type을 `number`로 했을 때 input 안에 up, down 버튼이 생긴다.
+- 제거하는 법 : 전역 css 파일에 아래 코드를 추가해준다.
+```css
+input[type="number"]::-webkit-outer-spin-button,
+input[type="number"]::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+```
--- web/src/components/CostPicker.js
+++ web/src/components/CostPicker.js
@@ -6,11 +6,12 @@ const CostPickerWrapper = styled.div`
   position: absolute;
   top: 3.8rem;
   width: 45rem;
-  height: 34rem;
+  height: 22rem;
   background: #ffffff;
   box-shadow: rgba(0, 0, 0, 0.28) 0px 8px 28px;
   border-radius: 0.8rem;
-  font-size: 1.4rem;
+  font-size: 2rem;
+  font-family: ${styles.normalFont};
 `;
 const RangeSliderContainer = styled.div`
   position: relative;
@@ -52,6 +53,65 @@ const RangeButton = styled.button`
   outline: none;
   user-select: none;
 `;
+const CostInputContainer = styled.div`
+  position: relative;
+  top: 7rem;
+  margin: 0 auto;
+  width: 32rem;
+`;
+const CostInputWrapper = styled.div`
+  position: absolute;
+  display: inline-block;
+  ${props => (props.isLeft ? "left: 0" : "right: 0")}
+  width: 14.2rem;
+  height: 4rem;
+  border: 1px solid
+    ${props => (props.visibleFocus ? styles.primaryColor : styles.borderColor)};
+  border-radius: 0.4rem;
+`;
+const BeforeCostInput = styled.div`
+  position: relative;
+  display: inline-block;
+  width: 3rem;
+  height: 4rem;
+  line-height: 4rem;
+  text-align: center;
+  font-size: 1.6rem;
+  font-family: ${styles.normalFont};
+  font-weight: bold;
+`;
+const CostInput = styled.input`
+  position: relative;
+  display: inline-block;
+  width: 10rem;
+  height: 4rem;
+  border: none;
+  outline: none;
+  padding: 0 0.5rem;
+  font-size: 1.6rem;
+`;
+const BetweenCostInput = styled.div`
+  position: absolute;
+  left: 50%;
+  transform: translateX(-50%);
+  height: 4.2rem;
+  line-height: 4.2rem;
+  font-family: ${styles.symbolFont};
+`;
+const Button = styled.div`
+  z-index: 5;
+  position: absolute;
+  ${props =>
+    props.type === "delete"
+      ? "left: 2rem;"
+      : "right: 2rem; color: " + styles.primaryColor + ";"}
+  bottom: 2.5rem;
+  cursor: pointer;
+  font-family: ${styles.handWrittenFont};
+  &:hover {
+    text-decoration: underline;
+  }
+`;
 
 const CostPicker = () => {
   const [leftPos, setLeftPos] = useState(0);
@@ -60,12 +120,22 @@ const CostPicker = () => {
   const [rightMouseStart, setRightMouseStart] = useState(undefined);
   const [leftButtonStart, setLeftButtonStart] = useState(0);
   const [rightButtonStart, setRightButtonStart] = useState(0);
+  const [focusLeftInput, setFocusLeftInput] = useState(false);
+  const [focusRightInput, setFocusRightInput] = useState(false);
 
   const handleMouseDownForLeft = e => {
+    const img = new Image();
+    img.src =
+      "data:image/gif;base64,R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs=";
+    e.dataTransfer.setDragImage(img, 0, 0);
     setLeftButtonStart(leftPos);
     setLeftMouseStart(e.clientX);
   };
   const handleMouseDownForRight = e => {
+    const img = new Image();
+    img.src =
+      "data:image/gif;base64,R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs=";
+    e.dataTransfer.setDragImage(img, 0, 0);
     setRightButtonStart(rightPos);
     setRightMouseStart(e.clientX);
   };
@@ -93,7 +163,12 @@ const CostPicker = () => {
   };
 
   return (
-    <CostPickerWrapper onMouseMove={handleMouseMove} onMouseUp={handleMouseUp}>
+    <CostPickerWrapper
+      onClick={() => {
+        setFocusRightInput(false);
+        setFocusLeftInput(false);
+      }}
+    >
       <RangeSliderContainer>
         <RangeSliderBar>
           <RangeSliderBarBetweenButtons
@@ -101,13 +176,59 @@ const CostPicker = () => {
             rangeBarWidth={rightPos - leftPos}
           />
         </RangeSliderBar>
-        <RangeButton onMouseDown={handleMouseDownForLeft} left={leftPos}>
+        <RangeButton
+          draggable="true"
+          onDragStart={handleMouseDownForLeft}
+          onDrag={handleMouseMove}
+          onDragEnd={handleMouseUp}
+          left={leftPos}
+        >
           |||
         </RangeButton>
-        <RangeButton onMouseDown={handleMouseDownForRight} left={rightPos}>
+        <RangeButton
+          draggable="true"
+          onDragStart={handleMouseDownForRight}
+          onDrag={handleMouseMove}
+          onDragEnd={handleMouseUp}
+          left={rightPos}
+        >
           |||
         </RangeButton>
       </RangeSliderContainer>
+      <CostInputContainer>
+        <CostInputWrapper
+          onClick={e => {
+            setFocusRightInput(false);
+            setFocusLeftInput(true);
+            e.stopPropagation();
+          }}
+          isLeft={true}
+          visibleFocus={focusLeftInput}
+        >
+          <BeforeCostInput>₩</BeforeCostInput>
+          <CostInput
+            type="number"
+            onChange={() => {
+              console.log(123);
+            }}
+          ></CostInput>
+        </CostInputWrapper>
+        <BetweenCostInput>-</BetweenCostInput>
+        <CostInputWrapper
+          onClick={e => {
+            setFocusLeftInput(false);
+            setFocusRightInput(true);
+            e.stopPropagation();
+          }}
+          isLeft={false}
+          visibleFocus={focusRightInput}
+        >
+          <BeforeCostInput>₩</BeforeCostInput>
+          <CostInput type="number"></CostInput>
+        </CostInputWrapper>
+      </CostInputContainer>
+      <Button type={"delete"}>삭제</Button>
+      <Button type={"save"}>저장</Button>
     </CostPickerWrapper>
   );
 };
